1c2efb243b6611dafa86e622d789c451ed132fae
Quest Yarbrough <quest@yarbrough.net>
2021-10-17 09:45:45 -0700

    Add high-level API tests over records endpoint
    Doesn't test full set of sort endpoints, but sorting functionality
    is already covered by CLI unit tests

--- test/hw/rest_parser/cli_test.clj
+++ test/hw/rest_parser/cli_test.clj
@@ -51,6 +51,38 @@
              ;; https://www.youtube.com/watch?v=QyrDgEz3DR0
              (trim-lines expected-table))))))
 
+(def merged-file-records
+  '({:LastName "Mcclure"
+     :FirstName "Joey"
+     :Email "joey.mc@gmail.com"
+     :FavoriteColor "magenta"
+     :DateOfBirth #inst "1991-03-22T08:00:00.000-00:00"}
+    {:LastName "Fitzpatrick"
+     :FirstName "Donna"
+     :Email "donna@fitzpatrick.com"
+     :FavoriteColor "blue"
+     :DateOfBirth #inst "1972-08-10T07:00:00.000-00:00"}
+    {:LastName "Morris"
+     :FirstName "Tyler"
+     :Email "tyler.morris@example.com"
+     :FavoriteColor "orange"
+     :DateOfBirth #inst "1951-08-06T07:00:00.000-00:00"}
+    {:LastName "Stewart"
+     :FirstName "Dora"
+     :Email "dora.stewart@example.com"
+     :FavoriteColor "purple"
+     :DateOfBirth #inst "1976-09-06T07:00:00.000-00:00"}
+    {:LastName "Pierce"
+     :FirstName "Dennis"
+     :Email "dennis.pierce@example.com"
+     :FavoriteColor "black"
+     :DateOfBirth #inst "1984-09-07T07:00:00.000-00:00"}
+    {:LastName "Price"
+     :FirstName "Lucille"
+     :Email "lucille.price@example.com"
+     :FavoriteColor "silver"
+     :DateOfBirth #inst "1946-01-01T08:00:00.000-00:00"}))
+
 (deftest run-test
   (testing "Basic CSV record loading returns expected results"
     (is (= (run {:filepaths "data/basic_record.csv"})
@@ -61,36 +93,7 @@
 
   (testing "Merged file records return expected results"
     (is (= (run {:filepaths "data/basic_record.csv,data/basic_record.psv,data/basic_record.ssv"})
-           '({:LastName "Mcclure"
-              :FirstName "Joey"
-              :Email "joey.mc@gmail.com"
-              :FavoriteColor "magenta"
-              :DateOfBirth #inst "1991-03-22T08:00:00.000-00:00"}
-             {:LastName "Fitzpatrick"
-              :FirstName "Donna"
-              :Email "donna@fitzpatrick.com"
-              :FavoriteColor "blue"
-              :DateOfBirth #inst "1972-08-10T07:00:00.000-00:00"}
-             {:LastName "Morris"
-              :FirstName "Tyler"
-              :Email "tyler.morris@example.com"
-              :FavoriteColor "orange"
-              :DateOfBirth #inst "1951-08-06T07:00:00.000-00:00"}
-             {:LastName "Stewart"
-              :FirstName "Dora"
-              :Email "dora.stewart@example.com"
-              :FavoriteColor "purple"
-              :DateOfBirth #inst "1976-09-06T07:00:00.000-00:00"}
-             {:LastName "Pierce"
-              :FirstName "Dennis"
-              :Email "dennis.pierce@example.com"
-              :FavoriteColor "black"
-              :DateOfBirth #inst "1984-09-07T07:00:00.000-00:00"}
-             {:LastName "Price"
-              :FirstName "Lucille"
-              :Email "lucille.price@example.com"
-              :FavoriteColor "silver"
-              :DateOfBirth #inst "1946-01-01T08:00:00.000-00:00"})))))
+           merged-file-records))))
 
 (def minimal-unsorted-records
   "Small handrolled set to guarantee correct sorting"
--- test/hw/rest_parser/server_test.clj
+++ test/hw/rest_parser/server_test.clj
@@ -1,7 +1,68 @@
 (ns hw.rest-parser.server-test
-  (:require [environ.core :as environ]
+  (:require [aleph.http :as http]
+            [byte-streams :as bs]
+            [cheshire.core :as json]
+            [environ.core :as environ]
             [clojure.edn :as edn]
+            [clojure.instant :as inst]
             [clojure.string :as str]
             [clojure.test :refer :all]
             [hw.rest-parser.server :refer :all]
-            [malli.instrument :as mi]))
+            [hw.rest-parser.cli :refer [with-env] :as cli]
+            [hw.rest-parser.cli-test :as cli-test]
+            [malli.instrument :as mi]
+            [mount.core :as mount]))
+
+(defn record->csv
+  "Maps record map into comma separated string"
+  [record]
+  (->> (select-keys record cli/row-headers)
+       (map (comp cli/render-view-cast-to-string
+                  second))
+       (str/join #", ")))
+
+(deftest record->csv-test
+  (is (= "Mcclure, Joey, joey.mc@gmail.com, magenta, 3/22/1991"
+         (record->csv {:LastName "Mcclure" :FirstName "Joey" :Email "joey.mc@gmail.com"
+                       :FavoriteColor "magenta" :DateOfBirth #inst "1991-03-22T08:00:00.000-00:00"}))))
+
+(defn post-record
+  "Given base uri to host, POSTs records & asserts return was successful"
+  [base-uri records]
+  (let [{:keys [status]} @(http/post (str base-uri "/records")
+                                     {:body records})]
+    (is (= status 200) "Server should accept well-formed records via POST")))
+
+(defn get-records
+  "Given base-uri & sort-type (color, birthdate, name), return records from server"
+  [base-uri sort-type]
+  (let [{:keys [status body]} @(http/get (str base-uri "/records/" sort-type))]
+    (is (= status 200) "Server should accept well-formed records via POST")
+    (-> (bs/to-string body)
+        (json/decode true))))
+
+(def localhost-3000 "http://localhost:3000")
+
+(deftest server-test
+  (testing "Webserver inits on correct port & responds to /health"
+    (with-env {:webserver-port "5000"}
+      (mount/stop)
+      (-main)
+      (is (-> @(http/get "http://localhost:5000/health")
+              :status
+              (= 204)))))
+
+  (testing "Webserver accepts POST records & returns records"
+    (with-env {:webserver-port "3000"}
+      (mount/stop)
+      (-main)
+
+      (doseq [record cli-test/merged-file-records]
+        (->> (record->csv record)
+             (post-record localhost-3000)))
+
+      (is (= (->> (get-records localhost-3000 "color")
+                  (map #(update % :DateOfBirth inst/read-instant-date))
+                  (into #{}))
+             (->> cli-test/merged-file-records
+                  (into #{})))))))
